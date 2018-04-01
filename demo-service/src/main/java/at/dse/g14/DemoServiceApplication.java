36de81108b7ec4980c13238a56227e4ef9306e73
Michael Sober <michael.sober@ymail.com>
2018-04-01 15:45:16 +0200

    Dockerize initial project setup, fix config
    Created dockerfiles for each module. Created docker-compose file. Changed some config files to make eureka, spring boot, feign work. Remove out folder.

--- demo-service/src/main/java/at/dse/g14/DemoServiceApplication.java
+++ demo-service/src/main/java/at/dse/g14/DemoServiceApplication.java
@@ -2,6 +2,7 @@ package at.dse.g14;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Lukas Baronyai
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoServiceApplication {
 
   public static void main(String[] args) {
