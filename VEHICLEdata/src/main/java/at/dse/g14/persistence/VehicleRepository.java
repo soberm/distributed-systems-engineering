a1a68313651221d83f3e816a094c119a6eb46779
Lukas <lukas.baronyai@gmail.com>
2018-04-24 17:11:40 +0200

    add EmergencyServiceRepository and add missing annotation to existing repos

--- VEHICLEdata/src/main/java/at/dse/g14/persistence/VehicleRepository.java
+++ VEHICLEdata/src/main/java/at/dse/g14/persistence/VehicleRepository.java
@@ -3,12 +3,14 @@ package at.dse.g14.persistence;
 import at.dse.g14.commons.dto.Vehicle;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Lukas Baronyai
  * @version ${buildVersion}
  * @since 1.0.0
  */
+@Repository
 public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
 
   List<Vehicle> findAllByManufacturer_Id(long id);
