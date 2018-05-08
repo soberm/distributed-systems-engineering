416074f23b091aa9d5398abe9e747c70754f30fc
Michael Sober <michael.sober@ymail.com>
2018-05-08 20:42:30 +0200

    Added simple tests for VehicleTrackService

--- V2Itracker/src/test/java/at/dse/g14/service/impl/VehicleTrackServiceTest.java
+++ V2Itracker/src/test/java/at/dse/g14/service/impl/VehicleTrackServiceTest.java
@@ -3,6 +3,8 @@ package at.dse.g14.service.impl;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import at.dse.g14.entity.GpsPoint;
+import at.dse.g14.entity.VehicleTrack;
 import at.dse.g14.service.IVehicleTrackService;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Test;
@@ -20,17 +22,29 @@ public class VehicleTrackServiceTest {
 
   @Test
   public void save_validVehicleTrack_shouldPersist(){
-    //TODO
+    VehicleTrack vehicleTrack = buildValidVehicleTrack(0L);
+    assertThat(vehicleTrackService.save(vehicleTrack), is(vehicleTrack));
   }
 
   @Test
   public void update_validVehicleTrack_shouldPersist(){
-    //TODO
+    Long id = 0L;
+    VehicleTrack vehicleTrack = buildValidVehicleTrack(id);
+    VehicleTrack savedVehicleTrack = vehicleTrackService.save(vehicleTrack);
+    assertThat(savedVehicleTrack, is(vehicleTrack));
+    savedVehicleTrack.setCrashEvent(true);
+    vehicleTrackService.update(savedVehicleTrack);
+    assertThat(vehicleTrackService.findOne(id), is(savedVehicleTrack));
+    vehicleTrackService.delete(id); //cleanup
   }
 
   @Test
   public void delete_validVehicleTrack_shouldPersist(){
-    //TODO
+    Long id = 0L;
+    VehicleTrack vehicleTrack = buildValidVehicleTrack(id);
+    assertThat(vehicleTrackService.save(vehicleTrack), is(vehicleTrack));
+    vehicleTrackService.delete(id);
+    assertThat(vehicleTrackService.findOne(id), is(nullValue()));
   }
 
   @Test
@@ -41,6 +55,21 @@ public class VehicleTrackServiceTest {
   @Test
   public void findAll_emptyDatabase_shouldReturnEmptyList() {
     assertThat(vehicleTrackService.findAll(), IsEmptyCollection.empty());
+  }
+
+  public VehicleTrack buildValidVehicleTrack(Long id){
+    return VehicleTrack.builder()
+        .id(id)
+        .vin("W0L000051T2123456")
+        .modelType("Opel")
+        .passengers(4)
+        .location(new GpsPoint(20, 20))
+        .speed(130)
+        .distanceVehicleAhead(20)
+        .distanceVehicleBehind(15)
+        .nearCrashEvent(false)
+        .crashEvent(false)
+        .build();
   }
 
 }
