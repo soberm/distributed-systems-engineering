eebb83c5f7ca8a38834e4a3a42855a2abe08ed4d
Michael Sober <michael.sober@ymail.com>
2018-05-09 20:10:42 +0200

    Integrated Angular Material and CDK

--- StoreFrontUI/src/app/app.module.ts
+++ StoreFrontUI/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
@@ -12,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
