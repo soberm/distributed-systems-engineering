8e0d2e592823464c0ba7c76cc055dd1bd36704d3
Michael Sober <michael.sober@ymail.com>
2018-05-18 11:30:40 +0200

    Added spring integration for google pub sub

---
+++ V2Itracker/src/main/java/at/dse/g14/config/PubSubConfig.java
@@ -0,0 +1,31 @@
+package at.dse.g14.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
+import org.springframework.cloud.gcp.pubsub.integration.AckMode;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
+
+@Configuration
+public class PubSubConfig {
+
+  @Bean
+  public PubSubInboundChannelAdapter messageChannelAdapter(
+      @Qualifier("vehicleTrackInputChannel") MessageChannel inputChannel,
+      PubSubOperations pubSubTemplate) {
+    PubSubInboundChannelAdapter adapter =
+        new PubSubInboundChannelAdapter(pubSubTemplate, "v2i-tracker");
+    adapter.setOutputChannel(inputChannel);
+    adapter.setAckMode(AckMode.MANUAL);
+    return adapter;
+  }
+
+  @Bean
+  public MessageChannel vehicleTrackInputChannel() {
+    return new DirectChannel();
+  }
+
+}
