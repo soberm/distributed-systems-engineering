70b36454e211c968ccc8fe51eeaa0e4005e978a5
Michael Sober <michael.sober@ymail.com>
2018-05-19 19:32:40 +0200

    Created initial GOVstat project

---
+++ GOVstat/src/main/java/at/dse/g14/GOVStatApplication.java
@@ -0,0 +1,24 @@
+package at.dse.g14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Michael Sober
+ * @version ${buildVersion}
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableFeignClients
+@EnableDiscoveryClient
+public class GOVStatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GOVStatApplication.class, args);
+  }
+
+}
