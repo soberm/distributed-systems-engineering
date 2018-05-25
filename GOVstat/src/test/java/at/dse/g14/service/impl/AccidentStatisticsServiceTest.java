8192dcaa050519e29742df9b4845ce2d8415e0df
Michael Sober <michael.sober@ymail.com>
2018-05-25 13:01:38 +0200

    Added empty test class

---
+++ GOVstat/src/test/java/at/dse/g14/service/impl/AccidentStatisticsServiceTest.java
@@ -0,0 +1,19 @@
+package at.dse.g14.service.impl;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AccidentStatisticsServiceTest {
+
+    @Test
+    public void test() throws Exception {
+        System.out.println("Executed test");
+        //TODO
+    }
+
+}
