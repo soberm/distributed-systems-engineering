0e9eedf1279fb068ab9a7e01418e09d0a4ee8608
Michael Sober <michael.sober@ymail.com>
2018-05-26 11:12:18 +0200

    Implemented message handling

---
+++ V2Itracker/src/main/java/at/dse/g14/config/V2ITrackerApplicationConfig.java
@@ -0,0 +1,14 @@
+package at.dse.g14.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class V2ITrackerApplicationConfig {
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+}
--- V2Itracker/src/main/java/at/dse/g14/messaging/VehicleTrackMessageHandler.java
+++ V2Itracker/src/main/java/at/dse/g14/messaging/VehicleTrackMessageHandler.java
@@ -1,10 +1,13 @@
 package at.dse.g14.messaging;
 
+import at.dse.g14.commons.dto.VehicleTrackDTO;
+import at.dse.g14.commons.service.exception.ServiceException;
+import at.dse.g14.entity.VehicleTrack;
 import at.dse.g14.service.IVehicleTrackService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
@@ -12,30 +15,44 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 
+import java.io.IOException;
+
 @Slf4j
 @MessageEndpoint
 public class VehicleTrackMessageHandler implements MessageHandler {
 
-  @Autowired
-  ObjectMapper objectMapper;
+  private final ObjectMapper objectMapper;
+  private final ModelMapper modelMapper;
+  private final IVehicleTrackService vehicleTrackService;
 
-  @Autowired
-  private IVehicleTrackService vehicleTrackService;
+  public VehicleTrackMessageHandler(
+      ObjectMapper objectMapper,
+      ModelMapper modelMapper,
+      IVehicleTrackService vehicleTrackService) {
+    this.objectMapper = objectMapper;
+    this.modelMapper = modelMapper;
+    this.vehicleTrackService = vehicleTrackService;
+  }
 
   @Override
   @ServiceActivator(inputChannel = "vehicleTrackInputChannel")
   public void handleMessage(Message<?> message) throws MessagingException {
-    log.info("VehicleTrackDTO-Message arrived! Payload: {}", message.getPayload());
-    /*try {
-      String payload = (String) message.getPayload();
-      VehicleTrackDTO vehicleTrack = objectMapper.readValue(payload, VehicleTrackDTO.class);
-      vehicleTrackService.update(vehicleTrack);*/
-      AckReplyConsumer consumer =
-          (AckReplyConsumer) message.getHeaders().get(GcpPubSubHeaders.ACKNOWLEDGEMENT);
+    log.info("VehicleTrack-Message arrived! Payload: {}", message.getPayload());
+    String payload = (String) message.getPayload();
+    AckReplyConsumer consumer =
+        (AckReplyConsumer) message.getHeaders().get(GcpPubSubHeaders.ACKNOWLEDGEMENT);
+    try {
+      VehicleTrackDTO vehicleTrackDTO = objectMapper.readValue(payload, VehicleTrackDTO.class);
+      vehicleTrackService.update(convertToEntity(vehicleTrackDTO));
+    } catch (ServiceException | IOException e) {
+      log.error("Could not handle VehicleTrack-Message. Message ignored.", e);
+    }
+    if (consumer != null) {
       consumer.ack();
-    /*} catch (ServiceException | IOException e) {
-      log.error("Could not handle received message.", e);
-    }*/
+    }
   }
 
+  private VehicleTrack convertToEntity(VehicleTrackDTO vehicleTrackDTO) {
+    return modelMapper.map(vehicleTrackDTO, VehicleTrack.class);
+  }
 }
