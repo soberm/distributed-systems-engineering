362182f6a3b59fea2dd2c459104e090577f5fdf3
Josef Wechselauer <wechselauer.josef@gmx.at>
2018-06-09 01:25:09 +0200

    stated kubernetes deployment

--- APIgateway/src/main/java/at/dse/g14/APIGatewayApplication.java
+++ APIgateway/src/main/java/at/dse/g14/APIGatewayApplication.java
@@ -3,6 +3,7 @@ package at.dse.g14;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -12,7 +13,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class APIGatewayApplication {
 
