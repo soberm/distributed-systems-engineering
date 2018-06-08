963a1c39716b4709a2b0a019779cd73484008e21
Josef Wechselauer <wechselauer.josef@gmx.at>
2018-06-08 10:54:16 +0200

    commented eureka client in vehicledata

--- VEHICLEdata/src/main/java/at/dse/g14/VehicleDataApplication.java
+++ VEHICLEdata/src/main/java/at/dse/g14/VehicleDataApplication.java
@@ -4,7 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -14,7 +13,7 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 @EnableCircuitBreaker
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class VehicleDataApplication {
 
   public static void main(String[] args) {
