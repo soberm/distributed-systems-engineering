cb2a1042594ed85aca175fad67d47ce5f19001e5
Michael Sober <michael.sober@ymail.com>
2018-06-08 17:09:18 +0200

    Created MessageHandler for upcoming Clearance and ArrivalEvents

---
+++ NOTYfier/src/main/java/at/dse/g14/messaging/ArrivalEventMessageHandler.java
@@ -0,0 +1,51 @@
+package at.dse.g14.messaging;
+
+import at.dse.g14.commons.dto.ArrivalEventDTO;
+import at.dse.g14.commons.service.exception.ServiceException;
+import at.dse.g14.service.IArrivalNotificationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+@Slf4j
+@MessageEndpoint
+public class ArrivalEventMessageHandler implements MessageHandler {
+
+  private final ObjectMapper objectMapper;
+  private final IArrivalNotificationService arrivalNotificationService;
+
+  public ArrivalEventMessageHandler(
+      ObjectMapper objectMapper, IArrivalNotificationService arrivalNotificationService) {
+    this.objectMapper = objectMapper;
+    this.arrivalNotificationService = arrivalNotificationService;
+  }
+
+  @Override
+  @ServiceActivator(inputChannel = "arrivalEventInputChannel")
+  public void handleMessage(Message<?> message) throws MessagingException {
+    log.info("ArrivalEvent-Message arrived! Payload: {}", message.getPayload());
+    String payload = (String) message.getPayload();
+    AckReplyConsumer consumer =
+        (AckReplyConsumer) message.getHeaders().get(GcpPubSubHeaders.ACKNOWLEDGEMENT);
+    try {
+      ArrivalEventDTO arrivalEventDTO = objectMapper.readValue(payload, ArrivalEventDTO.class);
+      handleArrivalEvent(arrivalEventDTO);
+    } catch (Exception e) {
+      log.error("Could not handle ArrivalEvent-Message. Message ignored.", e);
+    }
+    if (consumer != null) {
+      consumer.ack();
+    }
+  }
+
+  private void handleArrivalEvent(ArrivalEventDTO arrivalEventDTO) throws ServiceException {
+    log.info("Handling ArrivalEvent of {}", arrivalEventDTO);
+    // TODO: Handle ArrivalEvent
+  }
+}
---
+++ NOTYfier/src/main/java/at/dse/g14/messaging/ClearanceEventMessageHandler.java
@@ -0,0 +1,52 @@
+package at.dse.g14.messaging;
+
+import at.dse.g14.commons.dto.ClearanceEventDTO;
+import at.dse.g14.commons.service.exception.ServiceException;
+import at.dse.g14.service.IClearanceNotificationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+@Slf4j
+@MessageEndpoint
+public class ClearanceEventMessageHandler implements MessageHandler {
+
+  private final ObjectMapper objectMapper;
+  private final IClearanceNotificationService clearanceNotificationService;
+
+  public ClearanceEventMessageHandler(
+      ObjectMapper objectMapper, IClearanceNotificationService clearanceNotificationService) {
+    this.objectMapper = objectMapper;
+    this.clearanceNotificationService = clearanceNotificationService;
+  }
+
+  @Override
+  @ServiceActivator(inputChannel = "clearanceEventInputChannel")
+  public void handleMessage(Message<?> message) throws MessagingException {
+    log.info("ClearanceEvent-Message arrived! Payload: {}", message.getPayload());
+    String payload = (String) message.getPayload();
+    AckReplyConsumer consumer =
+        (AckReplyConsumer) message.getHeaders().get(GcpPubSubHeaders.ACKNOWLEDGEMENT);
+    try {
+      ClearanceEventDTO clearanceEventDTO =
+          objectMapper.readValue(payload, ClearanceEventDTO.class);
+      handleClearanceEvent(clearanceEventDTO);
+    } catch (Exception e) {
+      log.error("Could not handle ClearanceEvent-Message. Message ignored.", e);
+    }
+    if (consumer != null) {
+      consumer.ack();
+    }
+  }
+
+  private void handleClearanceEvent(ClearanceEventDTO clearanceEventDTO) throws ServiceException {
+    log.info("Handling {}", clearanceEventDTO);
+    // TODO: Handle ArrivalEvent
+  }
+}
