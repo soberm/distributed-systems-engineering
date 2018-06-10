b403c762018d78b62989e30368d352b494e017ab
Josef Wechselauer <wechselauer.josef@gmx.at>
2018-06-10 14:56:12 +0200

    example service discovery kubernetes in api gateway

--- NOTYfier/src/main/java/at/dse/g14/NotyfierApplication.java
+++ NOTYfier/src/main/java/at/dse/g14/NotyfierApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Michael Sober
@@ -13,8 +15,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableFeignClients
+@RestController
 //@EnableDiscoveryClient
 public class NotyfierApplication {
+
+  @RequestMapping(value = "/information")
+  public String available() {
+    return "Here is information from the NOTYfier service";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(NotyfierApplication.class, args);
