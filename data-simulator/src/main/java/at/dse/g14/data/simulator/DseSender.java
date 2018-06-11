cc6189554291110b8af7720af003f1507515cc2b
Lukas <lukas.baronyai@gmail.com>
2018-06-11 17:24:46 +0200

    finish Scenario 1 (random) incl arrival & clearance

--- data-simulator/src/main/java/at/dse/g14/data/simulator/DseSender.java
+++ data-simulator/src/main/java/at/dse/g14/data/simulator/DseSender.java
@@ -1,9 +1,13 @@
 package at.dse.g14.data.simulator;
 
+import at.dse.g14.commons.dto.ArrivalEventDTO;
+import at.dse.g14.commons.dto.ClearanceEventDTO;
 import at.dse.g14.commons.dto.EmergencyService;
 import at.dse.g14.commons.dto.Vehicle;
 import at.dse.g14.commons.dto.VehicleManufacturer;
 import at.dse.g14.commons.dto.VehicleTrackDTO;
+import at.dse.g14.data.simulator.config.PubSubConfig.ArrivalEventOutboundGateway;
+import at.dse.g14.data.simulator.config.PubSubConfig.ClearanceEventOutboundGateway;
 import at.dse.g14.data.simulator.config.PubSubConfig.VehicleTrackOutboundGateway;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,7 +29,9 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class DseSender {
 
-  private final VehicleTrackOutboundGateway gateway;
+  private final ClearanceEventOutboundGateway clearanceEventOutboundGateway;
+  private final ArrivalEventOutboundGateway arrivalEventOutboundGateway;
+  private final VehicleTrackOutboundGateway vehicleTrackOutboundGateway;
   private final ObjectMapper objectMapper;
   private final RestTemplate restTemplate;
 
@@ -34,67 +40,86 @@ public class DseSender {
 
   @Autowired
   public DseSender(
-      final VehicleTrackOutboundGateway gateway,
+      final ClearanceEventOutboundGateway clearanceEventOutboundGateway,
+      final VehicleTrackOutboundGateway vehicleTrackOutboundGateway,
+      final ArrivalEventOutboundGateway arrivalEventOutboundGateway,
       final ObjectMapper objectMapper,
       final RestTemplateBuilder restTemplateBuilder) {
 
+    this.clearanceEventOutboundGateway = clearanceEventOutboundGateway;
+    this.arrivalEventOutboundGateway = arrivalEventOutboundGateway;
+    this.vehicleTrackOutboundGateway = vehicleTrackOutboundGateway;
+
     this.restTemplate = restTemplateBuilder.build();
-    this.gateway = gateway;
     this.objectMapper = objectMapper;
   }
 
   public Vehicle createVehicle(final Vehicle vehicle) {
     log.info("create: {}", vehicle);
 
-    return restTemplate
-        .postForObject(vehicledataAddress + "/manufacturer/{vin}/vehicle",
-            vehicle,
-            Vehicle.class,
-            vehicle.getManufacturer().getId());
-
+    return restTemplate.postForObject(
+        vehicledataAddress + "/manufacturer/{vin}/vehicle",
+        vehicle,
+        Vehicle.class,
+        vehicle.getManufacturer().getId());
   }
 
   public List<Vehicle> getVehiclesOfManufacturer(final VehicleManufacturer manufacturer) {
     log.info("getVehiclesOfManufacturer: {}", manufacturer);
 
-    final Vehicle[] vehicles = restTemplate
-        .getForObject(vehicledataAddress + "/manufacturer/{vin}/vehicle",
+    final Vehicle[] vehicles =
+        restTemplate.getForObject(
+            vehicledataAddress + "/manufacturer/{vin}/vehicle",
             Vehicle[].class,
             manufacturer.getId());
 
     return Arrays.asList(vehicles);
-
   }
 
   public VehicleManufacturer createManufacturer(final VehicleManufacturer manufacturer) {
     log.info("create: {}", manufacturer);
 
-    return restTemplate.postForObject(vehicledataAddress + "/manufacturer/",
-        manufacturer,
-        VehicleManufacturer.class);
+    return restTemplate.postForObject(
+        vehicledataAddress + "/manufacturer/", manufacturer, VehicleManufacturer.class);
   }
 
   public VehicleManufacturer getManufacturer(final VehicleManufacturer manufacturer) {
     log.info("getManufacturer: {}", manufacturer);
-    return restTemplate.getForObject(vehicledataAddress + "/manufacturer/" + manufacturer.getId(),
-        VehicleManufacturer.class);
+    return restTemplate.getForObject(
+        vehicledataAddress + "/manufacturer/" + manufacturer.getId(), VehicleManufacturer.class);
   }
 
   public EmergencyService createEmergencyService(final EmergencyService emergencyService) {
     log.info("create: {}", emergencyService);
 
-    return restTemplate.postForObject(vehicledataAddress + "/emergencyService/",
-        emergencyService,
-        EmergencyService.class);
+    return restTemplate.postForObject(
+        vehicledataAddress + "/emergencyService/", emergencyService, EmergencyService.class);
   }
 
   public void sendTrackData(final VehicleTrackDTO vehicleTrack) {
     log.info("send: {}", vehicleTrack);
     try {
-      gateway.sendToPubsub(objectMapper.writeValueAsString(vehicleTrack));
+      vehicleTrackOutboundGateway.sendToPubsub(objectMapper.writeValueAsString(vehicleTrack));
     } catch (JsonProcessingException e) {
       log.error("Could not send LiveVehicleTrackDTO to PubSub.", e);
     }
   }
 
+  public void sendEvent(final ArrivalEventDTO arrivalEvent) {
+    log.info("send event {}", arrivalEvent);
+    try {
+      arrivalEventOutboundGateway.sendToPubsub(objectMapper.writeValueAsString(arrivalEvent));
+    } catch (JsonProcessingException e) {
+      log.error("Could not send ArrivalEventDTO to PubSub.", e);
+    }
+  }
+
+  public void sendEvent(final ClearanceEventDTO clearanceEvent) {
+    log.info("send event {}", clearanceEvent);
+    try {
+      clearanceEventOutboundGateway.sendToPubsub(objectMapper.writeValueAsString(clearanceEvent));
+    } catch (JsonProcessingException e) {
+      log.error("Could not send ClearanceEventDTO to PubSub.", e);
+    }
+  }
 }
--- data-simulator/src/main/java/at/dse/g14/data/simulator/config/PubSubConfig.java
+++ data-simulator/src/main/java/at/dse/g14/data/simulator/config/PubSubConfig.java
@@ -13,32 +13,42 @@ import org.springframework.messaging.MessageHandler;
 @Profile("!test")
 public class PubSubConfig {
 
-//  @Bean
-//  public PubSubInboundChannelAdapter messageChannelAdapter(
-//      @Qualifier("vehicleTrackInputChannel") MessageChannel inputChannel,
-//      PubSubOperations pubSubTemplate) {
-//    PubSubInboundChannelAdapter adapter =
-//        new PubSubInboundChannelAdapter(pubSubTemplate, "data-simulator");
-//    adapter.setOutputChannel(inputChannel);
-//    adapter.setAckMode(AckMode.AUTO);
-//    return adapter;
-//  }
-
-//  @Bean
-//  public MessageChannel vehicleTrackInputChannel() {
-//    return new DirectChannel();
-//  }
+  public static final String CHANNEL_EVENT_CLEARANCE = "clearanceEventInputChannel";
+  public static final String CHANNEL_EVENT_ARRIVAL = "arrivalEventInputChannel";
+  public static final String CHANNEL_TRACK = "arrivalEventInputChannel";
 
   @Bean
-  @ServiceActivator(inputChannel = "vehicleTrackInputChannel")
-  public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+  @ServiceActivator(inputChannel = CHANNEL_EVENT_CLEARANCE)
+  public MessageHandler clearanceEventMessageSender(PubSubOperations pubsubTemplate) {
+    return new PubSubMessageHandler(pubsubTemplate, "clearance-events");
+  }
+
+  @Bean
+  @ServiceActivator(inputChannel = CHANNEL_EVENT_ARRIVAL)
+  public MessageHandler arrivalEventMessageSender(PubSubOperations pubsubTemplate) {
+    return new PubSubMessageHandler(pubsubTemplate, "arrival-events");
+  }
+
+  @Bean
+  @ServiceActivator(inputChannel = CHANNEL_TRACK)
+  public MessageHandler trackMessageSender(PubSubOperations pubsubTemplate) {
     return new PubSubMessageHandler(pubsubTemplate, "vehicle-tracks");
   }
 
-  @MessagingGateway(defaultRequestChannel = "vehicleTrackInputChannel")
-  public interface VehicleTrackOutboundGateway {
+  @MessagingGateway(defaultRequestChannel = CHANNEL_EVENT_CLEARANCE)
+  public interface ClearanceEventOutboundGateway {
 
     void sendToPubsub(String text);
   }
 
+  @MessagingGateway(defaultRequestChannel = CHANNEL_EVENT_ARRIVAL)
+  public interface ArrivalEventOutboundGateway {
+
+    void sendToPubsub(String text);
+  }
+
+  @MessagingGateway(defaultRequestChannel = CHANNEL_TRACK)
+  public interface VehicleTrackOutboundGateway {
+    void sendToPubsub(String text);
+  }
 }
